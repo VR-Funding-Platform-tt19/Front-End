e17bd1c03dadba1e02c3266ac8f17cfe4d7ac178
Christopher Corvo <christophercorvo@gmail.com>
2020-11-15 16:57:14 -0500

    added file structure and initial notes

--- vr-funding-app/src/Component/SignUp.js
+++ vr-funding-app/src/Component/SignUp.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+// ----- Form Schema ------
+import { signUpFormSchema } from './FormSchemas/signUpFormSchema'
+
+// ------ initial state --------
+const initialState = {}
 
 const SignUp = () => {
+    
+    // -------- component state ----------
+    const [] = useState(initialState)
+
+    
+    // -------- Event Handlers ---------
+
+
+
     return (
         <div>
-            
+            {/*  Build out a sign up (registration) form. The form
+            should have:
+                - name
+                - password
+                - funder or fundraiser checkbox
+                - Any other pertinent info
+            */}
         </div>
     )
 }
