02ab399cec4b992eaa886618ca4305e55a7ede1b
kayode <kayode.richards@gmail.com>
2020-11-16 08:30:01 -0500

    commit

---
+++ vr-funding-app/src/Component/Store/Reducers/index.js
@@ -0,0 +1,15 @@
+// -------- Import Actions --------
+
+
+//--------- Initial State ----------
+const initialState = {}
+
+// -------- Reducer Function ---------
+export const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case:
+        return {}
+    default: 
+        return state;
+    }
+}
