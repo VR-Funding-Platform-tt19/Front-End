3d3ebb40c28f2f26fdf97cd0b1aca2c08bae7c6b
kayode <kayode.richards@gmail.com>
2020-11-16 18:41:11 -0500

    Update Project done and contact us forms

--- vr-funding-app/src/Component/ContactUs.js
+++ vr-funding-app/src/Component/ContactUs.js
@@ -1,11 +1,147 @@
 /////STRETCH//////
 
 import React from 'react'
+import {axiosWithAuth} from '../Utils/axiosWithAuth'
+import * as yup from 'yup'
+import {useHistory} from 'react-router-dom'
+
+
+const initialContactUsForm = {
+    name: '',
+    email: '',
+    message: '',
+}
+
+const initialErrors = {
+    name: '',
+    email: '',
+    message: '',
+}
+
+const initalDisabled = true
+
+const initialListOfContactForms = []
 
 const ContactUs = () => {
+    const [listOfContactForms, setListOfContactForms] = useState(initialListOfContactForms)
+    const [contactUsForm, setContactUsForm] = useState(initialContactUsForm)
+    const [errors, setErrors] = useState(initialErrors)
+
+    const history = useHistory()
+    //SCHEMA
+    const contactUsSchema = yup.object().shape({
+        name: yup
+            .string()
+            .required('Must include a name'),
+        email: yup
+            .string()
+            .email('Must be valid email address')
+            .required('Must include email address'),
+        message: yup
+            .string()
+            .required('Must include a message')
+})
+
+    const inputChange = (name, value) => {
+        yup
+            .reach(contactUsSchema, name)
+            .validate(value)
+            .then(valid => {
+                setErrors({
+                    ...errors, [name]: ''})
+            })
+            .catch(err => {
+                setErrors ({
+                    ...errors, [name]: err.errors[0]
+                })
+            })
+            
+            setContactUsForm({
+                ...contactUsForm, [name]: value
+            })
+           
+    }
+
+    const contactFormSubmit = () => {
+        const newContactForm = {
+            name: contactUsForm.name.trim(),
+            email: contactUsForm.email.trim(),
+            message: contactUsForm.message.trim(),
+        }
+        postNewContactForm(newContactForm)
+    }
+
+    //EVENT HANDLERS
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        inputChange(name, value)
+    }
+
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        contactFormSubmit()
+    }
+
+
+    const postNewContactForm = (newContactForm) => {
+        // console.log(listOfContactForms)
+        axios.post('https://reqres.in/api/ContactUs', newContactForm) // need the api endpoint
+            .then(response => {
+                setListOfContactForms(listOfContactForms.concat(response.data))
+                // console.log(listOfContactForms)
+            })
+            .catch(error => {
+                console.log('THIS IS YOUR ERROR------------>', error)
+                // console.log(listOfContactForms)
+            })
+            . finally(() => {
+                setcontactUsForm(initialContactUsForm)
+                // console.log(listOfContactForms)
+            })
+            
+    }
+    
+    useEffect(() => {
+        contactUsSchema.isValid(contactUsForm)
+        .then(valid => {
+            setDisabled(!valid);
+        })
+    }, [contactUsForm])
+
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <h1>Want to get in touch?</h1>
+                <Label>Name: {' '}</Label>
+                <input
+                name='name'
+                type='text'
+                placeholder='Please Enter Your Name'
+                value={contactUsForm.name}
+                onChange={handleChange}
+                />
+
+                <Label>Email: {' '}</Label>
+                <input
+                name='email'
+                type='text'
+                value={contactUsForm.email}
+                placeholder='Please Enter Your Email'
+                onChange={handleChange}
+                />
+
+                <Label>Message: {' '}</Label>
+                <input
+                name='message'
+                type='text'
+                value={contactUsForm.message}
+                placeholder='Please tell us your message'
+                onChange={handleChange}
+                />
+            </form>
+            <button className='sendButton' disabled={diabled} onClick={()=> history.push('/contact-confirmation')}>Send</button>
         </div>
     )
 }
