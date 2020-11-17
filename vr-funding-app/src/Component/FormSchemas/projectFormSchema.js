c9524eebbb17ad346aa795711a1fc7fb217cbc22
Christopher Corvo <christophercorvo@gmail.com>
2020-11-17 12:23:14 -0500

    fixed projectform schema bug

--- vr-funding-app/src/Component/FormSchemas/projectFormSchema.js
+++ vr-funding-app/src/Component/FormSchemas/projectFormSchema.js
@@ -3,19 +3,19 @@ import * as yup from 'yup';
 export const projectFormSchema = yup.object().shape({
     projectName: yup
         .string()
-        .required('Please enter a tile for your project.')
         .min(5, 'Title must be more than 5 characters')
-        .max(40, 'Title cant be more than 40 characters'),
+        .max(40, 'Title cant be more than 40 characters')
+        .required('Please enter a tile for your project.'),
     author: yup
         .string()
-        .required('Please enter an Author name.')
-        .min(3, 'Password must be at least 3 characters'),
+        .min(3, 'Password must be at least 3 characters')
+        .required('Please enter an Author name.'),
     description: yup
         .string()
-        .required('Please describe your fundraising project.')
         .min(100, 'Please give us more information about your project')
-        .max(400, 'Your description can not exceed 400 characters'),
+        .max(400, 'Your description can not exceed 400 characters')
+        .required('Please describe your fundraising project.'),
     fundingGoal: yup
-        .required('Please enter a funding amount')
-        .number(),
+        .number()
+        .required('Please enter a funding amount'),
 })
