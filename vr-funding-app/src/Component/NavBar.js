0bad6999ad97cac1fb305d4dd1f1872c879bff9f
cmirza <mail@cmirza.com>
2020-11-17 08:53:00 -0800

    initial navbar and footer

--- vr-funding-app/src/Component/NavBar.js
+++ vr-funding-app/src/Component/NavBar.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import styled from 'styled-components';
+import { Link, useHistory } from 'react-router-dom';
+
+const NavBar = styled.div`
+`
 
 const NavBar = () => {
+    const history = useHistory();
+
+    const logout () => {
+        window.localStorage.clear();
+        history.push('/login');
+    }
+
     return (
-        <div>
-            
-        </div>
+        <NavBar>
+            <div className='logo-title'>
+                <a href="index.html" class="title">SIXR VR Funding</a>
+                <nav>
+                    <ul>
+                        <Link to='/'>Home</Link>
+                        <Link to='/about'>About Us</Link>
+                        <Link to='/team'>Meet The Team</Link>
+                        <Link to='/dashboard'>Dashboard</Link> // ?
+                    </ul>
+                </nav>
+            </div>
+        </NavBar>
     )
 }
 
