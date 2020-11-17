86e0ee6b7158efe2c3cd907755790e890352861a
kayode <kayode.richards@gmail.com>
2020-11-17 18:03:30 -0500

    pulling changes

--- vr-funding-app/src/Component/ProjectCard.js
+++ vr-funding-app/src/Component/ProjectCard.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
-import {createContext} from 'react'
-import Project from './Project'
-
 // Note:
 // Project cards are rendered to the dashboard
 // Onlcick <Project> is rendered
 
-const ProjectContext = createContext()
 
-const ProjectCard = () => {
-    const [projectData, setProjectData] = useState([])
+const ProjectCard = (props) => {
+
+    const { project } = props
+    
     
     return (
-        <ProjectContext.Provider value={projectData}>
-
         <div>
             <h1>We are in Project Card</h1>
             <div>
@@ -24,8 +19,6 @@ const ProjectCard = () => {
                 <h4>Project Description: {project.description}</h4>
             </div>
         </div>
-        {projectData && <Project/>}
-        </ProjectContext.Provider>
     )
 }
 
